f5210cd05aaf3d4161de47dc2c914d70b882042a
Oluwafikayomi Soetan <soetandan@gmail.com>
2023-05-15 11:34:51 +0100

    adjusted function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -3,6 +3,7 @@
 
 
 def index_range(page: int, page_size: int) -> tuple:
+    """Returns start and end index ie pagination parameters"""
     start_index = (page - 1) * page_size
-    end_index = start_index + (page_size - 1)
-    return (start_index, end_index)
+    end_index = start_index + page_size
+    return start_index, end_index
